ec3e8f85bc661b741c046934ee98b667c93dde95
Vishal-770 <vishalpeace07@gmail.com>
2026-01-12 16:10:00 +0530

    refactor: move config initialization into state for WalletProvider

--- apps/nextjs-template/wallet/provider.tsx
+++ apps/nextjs-template/wallet/provider.tsx
@@ -4,17 +4,20 @@ import { getDefaultConfig, RainbowKitProvider } from "@rainbow-me/rainbowkit";
 import { WagmiProvider } from "wagmi";
 import { baseSepolia } from "wagmi/chains";
 import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
-
-const config = getDefaultConfig({
-  appName: "Inco NextJS Template",
-  projectId: process.env.NEXT_PUBLIC_PROJECT_ID || "inco-nextjs-template",
-  chains: [baseSepolia],
-  ssr: true,
-});
+import { useState } from "react";
 
 const queryClient = new QueryClient();
 
 const WalletProvider = ({ children }: { children: React.ReactNode }) => {
+  const [config] = useState(() =>
+    getDefaultConfig({
+      appName: "Inco NextJS Template",
+      projectId: process.env.NEXT_PUBLIC_PROJECT_ID || "inco-nextjs-template",
+      chains: [baseSepolia],
+      ssr: true,
+    })
+  );
+
   return (
     <WagmiProvider config={config}>
       <QueryClientProvider client={queryClient}>
